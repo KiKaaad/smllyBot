1f3ebbe1aa9e7e03995e06dcc9216440a6c5282d
Константин Кикидза <kikaaads@gmail.com>
2026-05-05 16:28:50 +0700

    Практически идеальная команда пинг

--- src/main/java/com/kika/smllybot/modules/ping/PrefixPing.java
+++ src/main/java/com/kika/smllybot/modules/ping/PrefixPing.java
@@ -1,42 +1,42 @@
 package com.kika.smllybot.modules.ping;
 
 import com.kika.smllybot.Main;
-import com.kika.smllybot.utils.I18n;
-import com.kika.smllybot.utils.I18nRequest;
+import com.kika.smllybot.styles.component.ping.PingComponent;
+import com.kika.smllybot.styles.text.ping.PingText;
+import com.kika.smllybot.utils.PrefixUtil;
+import net.dv8tion.jda.api.components.container.Container;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import java.util.Set;
 
-import static com.kika.smllybot.utils.colors.GREEN;
-
 public class PrefixPing extends ListenerAdapter {
+
+    Set<String> commands = Set.of("ping", "pong", "пинг", "понг");
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         if (event.getAuthor().isBot()) return;
 
-        String content = event.getMessage().getContentRaw().toLowerCase().trim();
-        String prefix = Main.prefixes[0].toLowerCase();
+        String command = PrefixUtil.getCommandBody(event.getMessage().getContentRaw(), Main.prefixes);
+        if (command == null) return;
 
-        Set<String> commands = Set.of("ping", "pong", "пинг", "понг");
-
-        if (content.startsWith(prefix)) {
-            content = content.substring(prefix.length()).trim();
-        }
-
-        if (commands.contains(content)) {
-
-            event.getJDA().getRestPing().queue((time) -> {
-                var getString = new I18nRequest("ru", "modules", "ping", "ping", "ping.response");
-                String response = I18n.get(getString);
-
-                event.getChannel().sendMessageFormat(response, time).queue();
-
-                var logReq = new I18nRequest("ru", "logger", "CommandUsed", "ping.used");
-                System.out.println(GREEN + I18n.get(logReq));
-
+        if (commands.contains(command)) {
+            PingService.fetchPing(event.getJDA(), data -> {
+                Container text = PingComponent.render(data);
+                event.getChannel().sendMessageComponents(text)
+                        .useComponentsV2(true)
+                        .queue();
             });
         }
+
+//        if (commands.contains(command)) {
+//            PingService.fetchPing(event.getJDA(), data -> {
+//                String text = PingText.render(data);
+//                event.getChannel().sendMessage(text).queue();
+//            });
+//        }
     }
 }
+
+
