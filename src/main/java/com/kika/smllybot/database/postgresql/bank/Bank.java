630db0c00e98e6911dee39da75ce9b7d8193ca5c
Константин Кикидза <kikaaads@gmail.com>
2026-05-05 16:26:17 +0700

    Будущий банк (мешок, ферма, ириски, ирис-коины)

---
+++ src/main/java/com/kika/smllybot/database/postgresql/bank/Bank.java
@@ -0,0 +1,3 @@
+package com.kika.smllybot.database.postgresql.bank;
+
+public record Bank(int id, long discordId, long irisCoin) {}
---
+++ src/main/java/com/kika/smllybot/database/postgresql/bank/BankTable.java
@@ -0,0 +1,10 @@
+package com.kika.smllybot.database.postgresql.bank;
+
+import com.kika.smllybot.database.postgresql.DatabaseManager;
+import java.sql.*;
+
+public class BankTable {
+
+    // BankTable
+
+}
