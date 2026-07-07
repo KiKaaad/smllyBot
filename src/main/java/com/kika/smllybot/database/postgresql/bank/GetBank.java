47d95ea753c35c7dd77c578f9c258b21cb1b87c5
Константин Кикидза <kikaaads@gmail.com>
2026-07-07 15:09:19 +0700

    не нужное

--- src/main/java/com/kika/smllybot/database/postgresql/bank/GetBank.java
+++
@@ -1,5 +0,0 @@
-package com.kika.smllybot.database.postgresql.bank;
-
-import java.sql.Timestamp;
-
-public record GetBank(int id, String name, long iris, long irisCoin, Timestamp lastFarm) {}
--- src/main/java/com/kika/smllybot/database/postgresql/user/GetUsers.java
+++
@@ -1,3 +0,0 @@
-package com.kika.smllybot.database.postgresql.user;
-
-public record GetUsers(int internalId, long discordId, String motto, String createdAt) {}
