a978fddedd3c0fd9c0562c7310fd06d47c6a6e9c
Константин Кикидза <kikaaads@gmail.com>
2026-07-07 14:53:03 +0700

    5

---
+++ src/main/java/com/kika/smllybot/modules/economy/Dice.java
@@ -0,0 +1,35 @@
+package com.kika.smllybot.modules.economy;
+
+import com.kika.smllybot.other.BaseCmd;
+import net.dv8tion.jda.api.components.container.Container;
+import net.dv8tion.jda.api.components.container.ContainerChildComponent;
+import net.dv8tion.jda.api.components.mediagallery.MediaGallery;
+import net.dv8tion.jda.api.components.mediagallery.MediaGalleryItem;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.utils.FileUpload;
+
+import java.io.InputStream;
+import java.util.Set;
+
+public class Dice extends BaseCmd {
+
+    public Dice() {
+        super(Set.of("кубик", "dice"));
+    }
+
+    InputStream stream = getClass().getResourceAsStream("/images/five.gif");
+
+    @Override
+    public void execute(MessageReceivedEvent event, String args) {
+
+        FileUpload upload = FileUpload.fromData(stream, "images/five.gif");
+        ContainerChildComponent response = MediaGallery.of(
+                MediaGalleryItem.fromFile(upload)
+        );
+
+        event.getChannel().sendMessageComponents(Container.of(response))
+                .useComponentsV2(true)
+                .queue();
+
+    }
+}
