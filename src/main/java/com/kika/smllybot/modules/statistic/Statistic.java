0f100b953bbc76c8f3dc26be0367401711c07cad
Константин Кикидза <kikaaads@gmail.com>
2026-07-14 23:27:11 +0700

    В статистику добавлены пользователи занесенные в базу данных бота. Также команду можно использовать в личных сообщениях

--- src/main/java/com/kika/smllybot/modules/statistic/Statistic.java
+++ src/main/java/com/kika/smllybot/modules/statistic/Statistic.java
@@ -1,7 +1,7 @@
 package com.kika.smllybot.modules.statistic;
 
-import com.kika.smllybot.other.BaseCmd;
 import com.kika.smllybot.modules.statistic.ui.StatisticUI;
+import com.kika.smllybot.other.BaseCmd;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDAInfo;
 import net.dv8tion.jda.api.components.container.Container;
@@ -15,21 +15,18 @@ import java.util.Set;
 public class Statistic extends BaseCmd {
 
         public Statistic() {
-                super(Set.of("статбот", "бостат", "statbot", "botstat"));
+                super(Set.of("статбот", "ботстат", "statbot", "botstat"));
         }
 
         @Override
-        public void execute(MessageReceivedEvent event, String arg) {
+        public Container execute(MessageReceivedEvent event, String arg) {
                 int shardTotal = JDA.ShardInfo.SINGLE.getShardTotal();
                 var jdaVersion = JDAInfo.VERSION;
                 var serversCount = (long) event.getJDA().getGuilds().size();
                 var userCount = event.getJDA().getUserCache().stream().count();
                 var botAvatarUrl = event.getJDA().getSelfUser().getAvatarUrl();
-
                 var botId = event.getJDA().getSelfUser().getId();
-                User user = event.getJDA().retrieveUserById(botId).complete();
-                User.Profile user1 = user.retrieveProfile().complete();
-                String botBannerUrl = user1.getBanner().getUrl(1024);
+                User bot = event.getJDA().getUserById(botId);
 
                 StatisticContext ctx = new StatisticContext(
                         jdaVersion,
@@ -37,15 +34,15 @@ public class Statistic extends BaseCmd {
                         serversCount,
                         userCount,
                         botAvatarUrl,
-                        botBannerUrl
+                        bot
                 );
 
                 Container response = StatisticUI.buildStatistic(ctx);
-
 
                 event.getChannel().sendMessageComponents(response)
                         .setAllowedMentions(EnumSet.noneOf(Message.MentionType.class))
                         .useComponentsV2(true)
                         .queue();
+            return response;
         }
 }
--- src/main/java/com/kika/smllybot/modules/statistic/StatisticContext.java
+++ src/main/java/com/kika/smllybot/modules/statistic/StatisticContext.java
@@ -1,10 +1,12 @@
 package com.kika.smllybot.modules.statistic;
 
+import net.dv8tion.jda.api.entities.User;
+
 public record StatisticContext(
         String jdaVersion,
         int shardTotal,
         long serversCount,
         long userCount,
         String botAvatarUrl,
-        String botBannerUrl
+        User user
 ) {}
