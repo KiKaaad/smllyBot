12979714c9166b83c711721e970c353cfedbe2c9
Константин Кикидза <kikaaads@gmail.com>
2026-07-14 23:10:51 +0700

    Удалены устаревшие (Deprecated) классы

--- src/main/java/com/kika/smllybot/database/UsersData.java
+++
@@ -1,7 +0,0 @@
-package com.kika.smllybot.database;
-
-import com.kika.smllybot.database.sql.bank.dto.BankAccount;
-import com.kika.smllybot.database.sql.user.dto.UserAccount;
-
-@Deprecated
-public record UsersData(UserAccount dbUser, BankAccount dbBank) {}
