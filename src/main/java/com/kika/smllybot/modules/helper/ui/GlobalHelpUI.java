8db1624698a2e2f5bb204d9f49a86b983adc213c
Константин Кикидза <kikaaads@gmail.com>
2026-07-27 13:53:58 +0700

    Больше помощи в команде хелп

--- src/main/java/com/kika/smllybot/modules/helper/ui/GlobalHelpUI.java
+++ src/main/java/com/kika/smllybot/modules/helper/ui/GlobalHelpUI.java
@@ -9,6 +9,39 @@ import java.util.List;
 
 public class GlobalHelpUI {
 
+    // TODO: Сделать кнопку с выбором с категории команд
+    public static Container defaultHelp() {
+
+        List<ContainerChildComponent> components = new ArrayList<>(10);
+
+        ContainerChildComponent header = TextDisplay.of("# \\🛟 Помощь по боту");
+        ContainerChildComponent main = TextDisplay.of("""
+                Здесь есть помощь по всем командам бота
+                Вводите - `хелп <название\\_модуля>`
+                ## \\🛰️ Модули
+                ### Пользователь
+                - анкета
+                   - девиз
+                - профиль
+                ### Топы
+                - гтоп
+                ### Экономика
+                - Ферма
+                - Мешок
+                ### Приватность
+                - Приватность
+                ### Статистика
+                - статбот
+                - гильдия
+                - пинг
+                """);
+
+        components.add(header);
+        components.add(main);
+
+        return Container.of(components);
+    }
+
     public static Container gtop() {
         List<ContainerChildComponent> components = new ArrayList<>(10);
 
@@ -28,9 +61,12 @@ public class GlobalHelpUI {
     public static Container statBot() {
         List<ContainerChildComponent> components = new ArrayList<>(10);
 
-        ContainerChildComponent header = TextDisplay.of("# \\📊 Статбот - статистика бота");
+        ContainerChildComponent header = TextDisplay.of("# \\📊 Статистика");
         ContainerChildComponent main = TextDisplay.of("""
-                - `статбот` - выводит статистику бота
+                - `статбот` - выводит статистику бота, отображает его нагрузку на ЦП, занятость ОЗУ и количество пользователей
+                -# P. S: При первом использовании нагрузка на ЦП может отобразить как 0%
+                - `гильдия` - выводит статистику гильдии, количество участников, онлайн сервера и его владельца
+                - `пинг` - выводит пинг от сервера бота до дискорда
                 """);
 
         components.add(header);
@@ -47,21 +83,14 @@ public class GlobalHelpUI {
                 - `анкета` - выводит глобальный профиль пользователя
                    - `анкета <юзернейм / айди>` - выводит профиль указанного пользователя
                    - `анкета` в ответ на чье-то сообщение - выводит профиль пользователя, на чье сообщение был произведен ответ
+                - `девиз` - отображает ваш текущий девиз
+                - `-девиз` - удалит ваш текущий девиз
+                - `девиз` ваш_девиз (на следующей строке) - установит новый девиз в анкете
+                - `профиль` - выводит локальный профиль со статистикой с гильдии
                 """);
 
         components.add(header);
         components.add(main);
-
-        return Container.of(components);
-    }
-
-    public static Container defaultHelp() {
-
-        List<ContainerChildComponent> components = new ArrayList<>(10);
-
-        ContainerChildComponent header = TextDisplay.of("# \\🛟 Помощь по боту");
-
-        components.add(header);
 
         return Container.of(components);
     }
