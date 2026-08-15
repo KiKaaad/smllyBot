c8d613eb5e217327a46b4bfe5c35530e4f0e4316
Константин Кикидза <kikaaads@gmail.com>
2026-08-15 19:44:04 +0700

    Полностью реализована команда помощи по боту

--- src/main/java/com/kika/smllybot/modules/helper/GlobalHelp.java
+++ src/main/java/com/kika/smllybot/modules/helper/GlobalHelp.java
@@ -1,23 +1,30 @@
 package com.kika.smllybot.modules.helper;
 
-import com.kika.smllybot.other.BaseCmd;
+import com.kika.smllybot.handlers.ButtonHandler;
 import com.kika.smllybot.modules.helper.ui.GlobalHelpUI;
+import com.kika.smllybot.other.BaseCmd;
+import com.kika.smllybot.utils.Interaction;
 import net.dv8tion.jda.api.components.container.Container;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 import java.util.Set;
 
-public class GlobalHelp extends BaseCmd {
+public class GlobalHelp extends BaseCmd implements ButtonHandler {
 
     public GlobalHelp() {
         super(Set.of("хелпа", "хелп", "хелпер"));
+    }
+    @Override
+    public String getButtonPrefix() {
+        return "help";
     }
 
     @Override
     public Container execute(MessageReceivedEvent event, String raw, String args) {
 
         if (args.isEmpty()) {
-            Container response = GlobalHelpUI.defaultHelp();
+            Container response = GlobalHelpUI.defaultHelp(event.getAuthor().getIdLong());
 
             event.getChannel().sendMessageComponents(response)
                     .useComponentsV2(true)
@@ -28,31 +35,67 @@ public class GlobalHelp extends BaseCmd {
         String[] parts = args.trim().split("\\s+");
         String subCommand = parts[0].toLowerCase();
 
-        switch (subCommand) {
-            case "гтоп" -> {
-                Container response = GlobalHelpUI.gtop();
+        // TODO: Добавить хелп <категория / команда>
 
-                event.getChannel().sendMessageComponents(response)
+        return null;
+    }
+
+    @Override
+    public void onButton(ButtonInteractionEvent event, String[] args) {
+        if (!Interaction.checkOwner(event, args)) return;
+        String[] componentId = event.getComponentId().split(":");
+        var discordId = event.getUser().getIdLong();
+
+        switch (componentId[1]) {
+            case "anketaAndProfile" -> {
+                var response = GlobalHelpUI.profileAndAnketa(discordId);
+
+                event.editComponents(response)
                         .useComponentsV2(true)
                         .queue();
             }
-            case "анкета" -> {
-                Container response = GlobalHelpUI.globalProfile();
+            case "tops" -> {
+                var response = GlobalHelpUI.tops(discordId);
 
-                event.getChannel().sendMessageComponents(response)
+                event.editComponents(response)
                         .useComponentsV2(true)
                         .queue();
             }
-            case "статистика" -> {
-                Container response = GlobalHelpUI.statBot();
+            case "economy" -> {
+                var response = GlobalHelpUI.economy(discordId);
 
-                event.getChannel().sendMessageComponents(response)
+                event.editComponents(response)
+                        .useComponentsV2(true)
+                        .queue();
+            }
+            case "statistic" -> {
+                var response = GlobalHelpUI.statistic(discordId);
+
+                event.editComponents(response)
+                        .useComponentsV2(true)
+                        .queue();
+            }
+            case "interactive" -> {
+                var response = GlobalHelpUI.interactive(discordId);
+
+                event.editComponents(response)
+                        .useComponentsV2(true)
+                        .queue();
+            }
+            case "other" -> {
+                var response = GlobalHelpUI.other(discordId);
+
+                event.editComponents(response)
+                        .useComponentsV2(true)
+                        .queue();
+            }
+            case "back" -> {
+                var response = GlobalHelpUI.defaultHelp(discordId);
+
+                event.editComponents(response)
                         .useComponentsV2(true)
                         .queue();
             }
         }
-
-        return null;
     }
 }
-// TODO: Доделать помощь по боту
