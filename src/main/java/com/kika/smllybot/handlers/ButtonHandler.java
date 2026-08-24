52b686c1f91b8c9f9447c1a49bcf45c5ad21dc73
Константин Кикидза <kikaaads@gmail.com>
2026-08-24 13:47:00 +0700

    Удалены не нужные классы, часть которых переписаны на котлин

--- src/main/java/com/kika/smllybot/handlers/ButtonHandler.java
+++
@@ -1,9 +0,0 @@
-package com.kika.smllybot.handlers;
-
-import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
-
-@Deprecated
-public interface ButtonHandler {
-    void onButton(ButtonInteractionEvent event, String[] args);
-    String getButtonPrefix();
-}
