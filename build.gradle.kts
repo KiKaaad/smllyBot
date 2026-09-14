c1433f074800c9b55f8a4eb66752776d3fdbc85a
Константин Кикидза <kikaaads@gmail.com>
2026-09-14 18:39:16 +0700

    🆙 chore(gradle): Обновлены зависимости

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "kikaaad.smlly"
-version = "0.6.3-beta-hotfix"
+version = "0.6.4-beta"
 
 java {
     toolchain {
@@ -40,7 +40,7 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:1.6.3")
     implementation("ch.qos.logback:logback-core:1.6.3")
     implementation("com.google.code.gson:gson:2.14.0")
-    implementation("net.dv8tion:JDA:6.5.0")
+    implementation("net.dv8tion:JDA:6.6.0")
     implementation("club.minnced:jda-ktx:0.15.0")
     implementation(libs.org.jetbrains.kotlin.kotlin.stdlib.jdk8)
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.11.0")
